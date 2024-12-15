9c78d9f0dfd6ca102308e8fc168b853e868d44d6
k1nnyyY <osman.haciev231@gmail.com>
2024-12-15 14:43:26 +0400

    add

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://angel-voice.ru",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
