04a859267a38c1b2c1499b16ec3cd4d67cf5db0a
k1nnyyY <osman.haciev231@gmail.com>
2024-12-16 19:51:32 +0400

    add

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,60 +2,64 @@ import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faMoon, faHeart, faFolder, faCircle } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "../ThemeProvider";
+
+const FooterContainer = styled.div`
+  width: 100%;
+  max-width: 360px;
+  position: fixed;
+  height: 7%;
+  bottom: 0;
+  left: 50%;
+  border-radius: 20px 20px 0 0;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 0;
+  background-color: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(15px);
+  -webkit-backdrop-filter: blur(15px);
+  border-top: 1px solid rgba(255, 255, 255, 0.2);
+`;
+
+const FooterIcon = styled.div`
+  font-size: 24px;
+  color: ${({ theme }) => theme.color};
+  cursor: pointer;
+
+  &:hover {
+    color: rgba(255, 255, 255, 0.8);
+  }
+`;
 
 const Footer = () => {
-  const Footer = styled.div`
-    width: 100%;
-    max-width: 360px;
-    position: fixed;
-    height:7%;
-    bottom: 0;
-    left: 50%;
-    border-radius: 20px 20px 0 0;
-    transform: translateX(-50%);
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 10px 0;
-    background-color: rgba(255, 255, 255, 0.2);
-    backdrop-filter: blur(15px);
-    -webkit-backdrop-filter: blur(15px);
-    border-top: 1px solid rgba(255, 255, 255, 0.2);
-  `;
+  const navigate = useNavigate();
+  const { toggleTheme } = useTheme();
 
-  const FooterIcon = styled.div`
-    font-size: 24px;
-    color: ${({ theme }) => theme.color};
-    cursor: pointer;
-
-    &:hover {
-      color: rgba(255, 255, 255, 0.8);
-    }
-  `;
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
+  const handleRedirect = (path) => {
+    navigate(path);
   };
 
-
   return (
-    <Footer>
-      <FooterIcon>
+    <FooterContainer>
+      <FooterIcon onClick={() => handleRedirect("/profile")}>
         <FontAwesomeIcon icon={faUser} />
       </FooterIcon>
-      <FooterIcon>
+      <FooterIcon onClick={() => handleRedirect("/card")}>
         <FontAwesomeIcon icon={faFolder} />
       </FooterIcon>
-      <FooterIcon>
-        <FontAwesomeIcon icon={faMoon} onClick={toggleTheme} />
+      <FooterIcon onClick={toggleTheme}>
+        <FontAwesomeIcon icon={faMoon} />
       </FooterIcon>
       <FooterIcon>
         <FontAwesomeIcon icon={faHeart} />
       </FooterIcon>
-      <FooterIcon>
+      <FooterIcon onClick={() => handleRedirect("/category")}>
         <FontAwesomeIcon icon={faCircle} />
       </FooterIcon>
-    </Footer>
+    </FooterContainer>
   );
 };
 
